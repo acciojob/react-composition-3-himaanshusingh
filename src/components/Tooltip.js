12a26d5bd844791107558b22191fca6a4a8ebd08
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-18 13:16:27 +0530

    commit

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,18 +3,10 @@ import React, { useState, useEffect } from "react";
 const Tooltip = ({ text, children }) => {
   const [toolTipText, setText] = useState("");
 
-  function handleOver() {
-    setText(text);
-  }
-
-  function handleOut() {
-    setText("");
-  }
-
   return (
     <>
-      <h2 className="tooltip" onMouseEnter={handleOver} onMouseLeave={handleOut}><div>{children}</div></h2>
-      {toolTipText && <p className="tooltip tooltiptext"><div>{toolTipText}</div></p>}
+      {toolTipText && <h2 className="tooltip">{text}</h2>}
+      <p className="tooltip tooltiptext" onMouseEnter={() => setText(text)} onMouseLeave={() => setText("")}>{children}</p>
     </>
   ); // prettier-ignore
 };
